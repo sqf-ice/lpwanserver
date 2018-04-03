de75a2643e976ff291f7747ceb587da56b171d32
Dan Schrimpsher <d.schrimpsher@cablelabs.com>
2018-04-03 09:50:59 -0500

    debug

--- rest/models/ICompany.js
+++ rest/models/ICompany.js
@@ -102,7 +102,7 @@ Company.prototype.pullCompanies = function( network ) {
     let me = this;
     return new Promise( async function(resolve, reject) {
         try {
-            let remoteCompanies = await modelAPI.retrieveNetwork.pullCompanies( network.networkTypeId );
+            let remoteCompanies = await modelAPI.networkTypeAPI.pullCompanies( network.networkTypeId );
             // var network = await modelAPI.networks.retrieveNetwork( networkId );
             // var proto = await modelAPI.networkProtocolAPI.getProtocol( network );
             // var dataAPI =  new NetworkProtocolDataAccess( modelAPI, "Pull Companies" );
